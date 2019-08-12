bfe33609039d2202e726881b8ee93e16d6883e04
Caleb Redd <caleb.redd@yahoo.com>
2019-08-11 19:57:47 -0700

    DRY code by making MovieCard.js functional for movielist and movie tabs

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// const movieData=[];
+import MovieCard from "./MovieCard";
 
 const Movie = props => {
   const [movie, setMovie] = useState();
   useEffect(() => {
     const id = props.match.params.id;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
 
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
@@ -29,26 +27,9 @@ const Movie = props => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard movie={movie} />
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([]);
@@ -29,27 +29,7 @@ const MovieList = props => {
 };
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <Link to={`/Movies/${movie.id}`}>
-        <h2>{title}</h2>
-      </Link>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+  return <MovieCard movie={movie} />;
 }
 
 export default MovieList;
