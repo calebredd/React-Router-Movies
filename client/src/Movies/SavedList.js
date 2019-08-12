38cc7ad7bc062bee29c6912ea580cc46e18220c2
Caleb Redd <caleb.redd@yahoo.com>
2019-08-11 20:03:54 -0700

    Added nav link functionality for saved movies

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -5,9 +5,13 @@ const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <Link to={`/Movies/${movie.id}`}>
+        <span className="saved-movie">{movie.title}</span>
+      </Link>
     ))}
-    <Link to={'/'}><div className="home-button">Home</div></Link>
+    <Link to={"/"}>
+      <div className="home-button">Home</div>
+    </Link>
   </div>
 );
 
